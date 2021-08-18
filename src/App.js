b1c97f84ca080e13ebe9c805787c0c806ba46644
EbenezerAnsere <82359996+EbenezerAnsere@users.noreply.github.com>
2021-08-18 14:29:10 +0000

    Final commit

--- src/App.js
+++ src/App.js
@@ -8,32 +8,19 @@ import Domain from './components/DomainSearch/Domain'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-    return ( <
-        div className = "App" >
+    return ( <div className = "App" >
 
-        { /* <img src={logo} className="App-logo" alt="logo" /> */ } <
-        BrowserRouter >
-        <
-        Switch >
-        <
-        Route path = "/Login"
-        component = { Login }
-        /> <
-        Route path = "/Forgetpassword"
-        component = { Forgetpassword }
-        /> <
-        Route path = "/Signup"
-        component = { Signup }
-        /> <
-        Route path = "/Home"
-        component = { Home }
-        /> <
-        Route path = "/Domain"
-        component = { Domain }
-        /> <
-        /Switch> <
-        /BrowserRouter> <
-        /div> 
+        { /* <img src={logo} className="App-logo" alt="logo" /> */}
+        <BrowserRouter>
+        <Switch>
+                <Route path="/Login" component={Login} />
+                <Route path="/Forgetpassword" component={Forgetpassword} />
+                <Route path="/Signup" component={Signup} />
+                <Route path="/" component={Home} />
+                <Route path = "/Domain" component = { Domain }/> 
+                </Switch> 
+                </BrowserRouter> 
+                </div> 
     );
 }
 export default App
